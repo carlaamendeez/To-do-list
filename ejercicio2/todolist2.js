b43527fbeead63407e923ae0ef3aced93414bbcb
carlaamendeez <carla.mendez.bonilla@gmail.com>
2025-10-02 19:02:36 +0200

    Update todolist2.js

--- ejercicio2/todolist2.js
+++ ejercicio2/todolist2.js
@@ -1,17 +1,13 @@
 document.getElementById("btnDescargar").addEventListener("click", async () => {
   try {
-    // Hacer fetch al API
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     const todos = await response.json();
 
-    // Tomar solo los 10 primeros
     const primerosDiez = todos.slice(0, 10);
 
-    // Seleccionar la lista
     const ul = document.getElementById("lista");
     ul.innerHTML = ""; // Limpiar lista antes de cargar
 
-    // Insertar cada ítem en <li>
     primerosDiez.forEach(todo => {
       const li = document.createElement("li");
       li.textContent = todo.title;
